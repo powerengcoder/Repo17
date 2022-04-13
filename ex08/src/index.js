4e33ff63455434af41d5b79b7f5df867dc9c373a
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-14 00:30:22 +0200

    Add ex 08

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,29 +1,53 @@
 
-const users = {
-   firstName: ["John", "Jane", "Jim", "Kate"] ,
-   lastName: ["Doe", "Doe", "Carrey", "Winslet"] ,
-   age: [24, 5, 54, 40] ,
-   gender: ["male", "female", "male", "female"] 
-};
+var users = [
+{
+  "firstName": "John",
+  "lastName": "Doe",
+  "age": 24,
+  "gender": "male"
+},
+{
+  "firstName": "Jane",
+  "lastName": "Doe",
+  "age": 5,
+  "gender": "female"
+},
+{
+  "firstName": "Jim",
+  "lastName": "Carrey",
+  "age": 54,
+  "gender": "male"
+},
+{
+  "firstName": "Kate",
+  "lastName": "Winslet",
+  "age": 40,
+  "gender": "female"
+}
 
+
+];
 function getUsers() {
-  var output = "";
-  for (i = 0; i < users.length; i++) {
-    console.log(users[i])
+  var output ="";
+  for(i=0; i<users.length; i++){
+    output = `${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender}`;
+    console.log(output);
+  }
+  return output;
+}
+function findUser (lastName, gender){
+  try {
+    var user = users.find(x => x.lastName === lastName && x.gender === gender);
+    var iFindUser = `${user.firstName} ${user.lastName} is ${user.age}, ${user.gender}`;
+    console.log(iFindUser);
+    return iFindUser;
+    }catch (error) {
+     
+      console.log(error);
+      console.log("Cannot read property 'firstName' of undefined");
   }
 }
 
-/*function findUser(lastName, gender) {
-  try {
-    var user = getUsers();
-    var iFindUser;
-    console.log(iFindUser);
-    return iFindUser;
-  } catch (error) {
-    return "Cannot read property 'firtName' of undefined";
-  }
-}*/
-
 getUsers();
-//findUser("Doe", 5);
-//module.exports = getUsers;
+findUser("Doeeee", "male");
+module.exports = getUsers;
